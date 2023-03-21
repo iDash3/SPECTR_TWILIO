ddfb50888a680694f37d2a8a7a71eab1a4504566
IsaacDash <idash3.b@gmail.com>
2023-03-21 12:41:34 -0600

    Template twilio

--- express/server.js
+++ express/server.js
@@ -1,23 +1,35 @@
+// Import necessary libraries
 const express = require("express");
-const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const bodyParser = require("body-parser");
+const twilio = require("twilio");
 
+// Initialize express app
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// Parse incoming requests as JSON
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Define endpoint for incoming messages from Twilio
+app.post("/whatsapp", (req, res) => {
+  const twiml = new twilio.twiml.MessagingResponse();
+  const message = req.body.Body;
+  const sender = req.body.From;
+
+  // Process incoming message
+  console.log(`Received message '${message}' from ${sender}`);
+
+  // Create response message
+  const response = `Thanks for your message! You said: ${message}`;
+
+  // Send response message
+  twiml.message(response);
+
+  // Send TwiML response back to Twilio
+  res.set("Content-Type", "text/xml");
+  res.send(twiml.toString());
 });
 
-app.post("/whats", (req, res) => {
-  const twiml = new MessagingResponse();
-
-  switch (req.body.body) {
-    case "hello":
-      twiml.message;
-      break;
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
